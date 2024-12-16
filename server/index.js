8844aa9643ba3604e7ab0b84c6e62182a4762efe
samraddhi <samraddhi829@gmail.com>
2024-12-16 21:30:14 +0530

    Describe the changes you made

--- server/index.js
+++ server/index.js
@@ -76,7 +76,7 @@ app.post('/login', async (req, res) => {
           httpOnly:true,
           sameSite:'None',
           secure:true,
-          domain: '.onrender.com',
+          domain: 'onrender.com',
         }).json({
           id: userDoc._id,
           username
